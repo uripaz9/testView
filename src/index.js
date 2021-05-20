569dd16e3a161ac1aee973ee1a9328c0f5817a02
uri paz <uripaz9@gmail.com>
2021-05-20 22:19:11 +0300

    codesandbox change

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Github</h1>
+<h1>Hello Vanilla! Gitgub</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
+<div>test</div>
 `;
